20175791146e610ed7f8ce0afd6542c933243548
bsy7716 <45975567+bsy7716@users.noreply.github.com>
2022-08-05 18:08:00 +0900

    Update content.js

--- js/content.js
+++ js/content.js
@@ -2,7 +2,8 @@ const btns = document.querySelectorAll(".btns li");
 const boxs = document.querySelectorAll("section article");
 let number = 0;
 // 버튼의 개수만큼 반복, 해당 요소 이벤트 등록
-for(i in btns){
+
+for(let i=0; i<btns.length; i++){
     // 각 버튼 클릭
     btns[i].addEventListener("click", (event) => {
         // on 클래스 추가 및 제거 로직
@@ -15,4 +16,4 @@ for(i in btns){
         boxs[number].classList.toggle("on");
 
     });
-};
+}
